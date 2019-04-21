4375cc6018e8752df7985e6f8edabd33b88ae9cc
Rafael Correa Alves <rafael.correa@sap.com>
2019-04-20 23:51:36 -0300

    Created Character class matching the previous test

---
+++ src/Character.ts
@@ -0,0 +1,11 @@
+export class Character{
+    health:number;
+    level:number;
+    alive:boolean;
+    
+    constructor(){
+        this.health=1000;
+        this.level=1;
+        this.alive=true;
+    }
+}
