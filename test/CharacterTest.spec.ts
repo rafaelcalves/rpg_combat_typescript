298c048832e94137044be004698a319ceed38a2d
Rafael Correa Alves <rafael.correa@sap.com>
2019-04-20 23:53:52 -0300

    Added tests for Character's attrs

--- test/CharacterTest.spec.ts
+++ test/CharacterTest.spec.ts
@@ -7,5 +7,11 @@ describe('Character', function(){
         it('should have health equals to 1000', function() {
             expect( character.health ).to.be.equals(1000);
         });
+        it('should have level equals to 1', function() {
+            expect( character.level ).to.be.equals(1);
+        });
+        it('should be alive', function() {
+            expect( character.alive ).to.be.equals(true);
+        });
     });
 });
