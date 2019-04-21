ab6cbac148f9112c241cdf26432921d753d58b46
Rafael Correa Alves <rafael.correa@sap.com>
2019-04-21 00:25:40 -0300

    Refactored Character to have protected attrs

--- src/Character.ts
+++ src/Character.ts
@@ -1,11 +1,23 @@
 export class Character{
-    health:number;
-    level:number;
-    alive:boolean;
+    protected health:number;
+    protected level:number;
+    protected alive:boolean;
     
     constructor(){
         this.health=1000;
         this.level=1;
         this.alive=true;
+    }
+    
+    public get getHealth() : number {
+        return this.health;
+    }
+    
+    public get getLevel() : number {
+        return this.level; 
+    }
+    
+    public get isAlive() : boolean {
+        return this.alive;
     }
 }
--- test/CharacterTest.spec.ts
+++ test/CharacterTest.spec.ts
@@ -5,13 +5,13 @@ describe('Character', function(){
     describe('a new one', function(){
         let character = new char.Character;
         it('should have health equals to 1000', function() {
-            expect( character.health ).to.be.equals(1000);
+            expect( character.getHealth ).to.be.equals(1000);
         });
         it('should have level equals to 1', function() {
-            expect( character.level ).to.be.equals(1);
+            expect( character.getLevel ).to.be.equals(1);
         });
         it('should be alive', function() {
-            expect( character.alive ).to.be.equals(true);
+            expect( character.isAlive ).to.be.equals(true);
         });
     });
 });
